a305eea4528afc5f294e3d95895b57464641dce0
Lara <75126217+little-evil-genius@users.noreply.github.com>
2022-01-11 23:48:31 +0100

    Create faceclaims_database.lang.php

---
+++ inc/languages/faceclaims_database.lang.php
@@ -0,0 +1,108 @@
+<?php
+
+// TEAM MELDUNG AUF DEM INDEX
+$l['newentry_faceclaims_database_headerbanner'] = '<div class="red_alert"><a href="modcp.php?action=faceclaims_database"><b>{1} {2} {3}</b> für die Avatarpersonendatenbank müssen freigeschaltet werden!</a></div>';
+
+// MODCP
+$l['faceclaims_database_modcp_nav'] = 'Avatarpersonen freischalten';
+$l['faceclaims_database_modcp_delete'] = 'Ablehnen';
+$l['faceclaims_database_modcp_accept'] = 'Annehmen';
+
+// HINZUFÜGEN & BEARBEITEN
+$l['faceclaims_database_add_nav'] = 'Avatarperson hinzufügen';
+$l['faceclaims_database_add_faceclaim'] = 'Name der Avatarperson';
+$l['faceclaims_database_add_faceclaim_desc'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam';
+$l['faceclaims_database_add_gender'] = 'Geschlecht der Avatarperson';
+$l['faceclaims_database_add_gender_desc'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam';
+$l['faceclaims_database_add_birthday'] = 'Geburtsjahr der Avatarperson';
+$l['faceclaims_database_add_birthday_desc'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam';
+$l['faceclaims_database_add_origin'] = 'Herkunft der Avatarperson';
+$l['faceclaims_database_add_origin_desc'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam';
+$l['faceclaims_database_add_haircolor'] = 'Haarfarbe der Avatarperson';
+$l['faceclaims_database_add_haircolor_desc'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam';
+$l['faceclaims_database_add_image'] = 'Bild von der Avatarperson';
+$l['faceclaims_database_add_image_desc'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam';
+$l['faceclaims_database_add_gallery'] = 'Link zu einer Galerie von der Avatarperson';
+$l['faceclaims_database_add_gallery_desc'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam';
+$l['faceclaims_database_add_special'] = 'Besonderheiten der Avatarperson';
+$l['faceclaims_database_add_special_desc'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam';
+$l['faceclaims_database_add_mediabase'] = 'Mediabase der Avatarperson';
+$l['faceclaims_database_add_mediabase_desc'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam';
+$l['faceclaims_database_add_button'] = 'Avatarperson eintragen';
+$l['faceclaims_database_edit_nav'] = 'Avatarperson bearbeiten';
+$l['faceclaims_database_edit_button'] = 'Avatarperson editieren';
+$l['faceclaims_database_add_error'] = 'Diese Avatarperson wurde schon eingetragen! Bitte trage eine andere Avatarperson ein.';
+
+// AVATARPERSON BIT
+$l['faceclaims_database_faceclaim_bit_age_limit'] = 'von <b>minimal {1} Jahre</b> bis <b>maximal {2} Jahre</b>';
+$l['faceclaims_database_faceclaim_bit_special'] = 'Diese Avatarperson besitzt folgende Besonderheiten:<br>{1}';
+$l['faceclaims_database_faceclaim_bit_special_none'] = 'Diese Avatarperson besitzt keine eingetragenen Besonderheiten!';
+$l['faceclaims_database_faceclaim_bit_mediabase'] = 'Diese Avatarperson besitzt folgende Mediabase:<br>{1}';
+$l['faceclaims_database_faceclaim_bit_mediabase_none'] = 'Diese Avatarperson besitzt keine eingetragenen Mediabase!';
+$l['faceclaims_database_faceclaim_bit_gallery'] = '<a href="{1}">Zur Galerie</a>';
+$l['faceclaims_database_faceclaim_bit_gallery_none'] = 'keine eingetragene Galerie!';
+
+// FILTERSEITEN
+$l['faceclaims_database_faceclaim_all'] = 'Alle Avatarpersonen';
+$l['faceclaims_database_faceclaim_title'] = 'Avatarpersonen';
+$l['faceclaims_database_faceclaim_count'] = '<strong>{1}</strong> {2} gefunden';
+$l['faceclaims_database_faceclaim_edit'] = 'Bearbeiten';
+$l['faceclaims_database_faceclaim_delete'] = 'Löschen';
+$l['faceclaims_database_faceclaim_none'] = 'Es wurde kein Avatarpersoneneintrag für diesen Filter gefunden!';
+
+// SPEZIELLE FILTERSEITE
+$l['faceclaims_database_filterpage_nav'] = 'Avatarpersonen speziell filtern';
+$l['faceclaims_database_filterpage_gender'] = 'Geschlecht:';
+$l['faceclaims_database_filterpage_origin'] = 'Herkunft:';
+$l['faceclaims_database_filterpage_haircolor'] = 'Haarfarbe:';
+$l['faceclaims_database_filterpage_age'] = 'Alter:';
+$l['faceclaims_database_filterpage_age_start'] = 'Zwischen';
+$l['faceclaims_database_filterpage_age_and'] = 'und';
+$l['faceclaims_database_filterpage_age_end'] = 'Jahre alt';
+$l['faceclaims_database_filterpage_button'] = 'Suche starten';
+$l['faceclaims_database_filterpage_name'] = 'Name:';
+$l['faceclaims_database_filterpage_firstletter'] = 'Anfangsbuchstabe';
+$l['faceclaims_database_filterpage_fullname'] = 'Ganzer Name';
+
+// HAUPTSEITE
+$l['faceclaims_database_mainpage_nav'] = 'Avatarpersonendatenbank';
+$l['faceclaims_database_mainpage_nav2'] = 'Hauptseite';
+$l['faceclaims_database_mainpage_title'] = 'Herzlich Willkommen in der foreninternen Avatarpersonendatenbank';
+$l['faceclaims_database_mainpage_desc'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br><br>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. ';
+
+// FILTER-MENÜ
+$l['faceclaims_database_menu'] = 'Menü';
+$l['faceclaims_database_menu_main'] = '» <a href="faceclaims_database.php?action=main">Hauptseite</a>';
+$l['faceclaims_database_menu_add'] = '» <a href="faceclaims_database.php?action=add">Avatarperson hinzufügen</a>';
+$l['faceclaims_database_menu_all'] = '» <a href="faceclaims_database.php?action=all">Alle Avatarpersonen</a>';
+$l['faceclaims_database_menu_filterpage'] = '» <a href="faceclaims_database.php?action=filter">Speziell filtern</a>';
+$l['faceclaims_database_menu_random'] = '» <a href="faceclaims_database.php?action=random">Zufällige Auswahl</a>';
+$l['faceclaims_database_menu_filters'] = 'Filteroptionen';
+$l['faceclaims_database_menu_filters_gender'] = 'nach Geschlecht';
+$l['faceclaims_database_menu_filters_gender_entry'] = "<tr><td class='trow1'>&raquo; <a href='faceclaims_database.php?filters={1}'>{2}</a> </td></tr>";
+$l['faceclaims_database_menu_filters_origin'] = 'nach Herkunft';
+$l['faceclaims_database_menu_filters_origin_entry'] = "<tr><td class='trow1'>&raquo; <a href='faceclaims_database.php?filters={1}'>{2}</a> </td></tr>";
+$l['faceclaims_database_menu_filters_haircolor'] = 'nach Haarfarbe';
+$l['faceclaims_database_menu_filters_haircolor_entry'] = "<tr><td class='trow1'>&raquo; <a href='faceclaims_database.php?filters={1}'>{2}</a> </td></tr>";
+$l['faceclaims_database_menu_filters_age'] = 'nach Alter';
+$l['faceclaims_database_menu_filters_age_entry'] = "<tr><td class='trow1'>&raquo; <a href='faceclaims_database.php?filters={1}-{2}'>{3} bis {2} Jahre</a> </td></tr>";
+
+// WEITERLEITUNGSSEITEN
+$l['faceclaims_database_redirect_add'] = 'Die Avatarperson wurde erfolgreich hinzugefügt! Du wirst nun auf die Hauptseite zurückgeleitet!';
+$l['faceclaims_database_redirect_delete'] = 'Du hast erfolgreich die Avatarperson gelöscht! Du wirst nun zurückgeleitet!';
+$l['faceclaims_database_redirect_edit'] = 'Du hast die Avatarperson erfolgreich bearbeitet! Du wirst nun auf die Hauptseite zurückgeleitet!';
+$l['faceclaims_database_redirect_random'] = 'Die zufällige Ausgabe von Avatarpersonen wurde vom Team deaktiviert! Du wirst nun auf die Hauptseite der Avatarpersonendatenbank zurückgeleitet!';
+$l['faceclaims_database_redirect_uninstall'] = 'Die Avatarpersonendatenbank wurde vom Team nicht installiert! Du wirst nun auf die Hauptseite vom Board zurückgeleitet!';
+$l['faceclaims_database_redirect_add_error'] = 'Du hast keine Berechtigung diese Avatarperson zu bearbeiten! Du wirst nun auf die Hauptseite der Avatarpersonendatenbank zurückgeleitet!';
+$l['faceclaims_database_redirect_edit_error'] = 'Du hast keine Berechtigung Avatarpersonen zur Datenbank hinzu zu fügen! Du wirst nun auf die Hauptseite der Avatarpersonendatenbank zurückgeleitet!';
+
+// MyALERTS
+$l['faceclaims_database_delete'] = '{1} hat deine Avatarperson <strong>"{4}"</strong> abgelehnt!';
+$l['myalerts_setting_faceclaims_database_delete'] = "Benachrichtigung bei Ablehnung vom Team bei von dir hinzugefügten Avatarpersonen für die Avatarpersonendatenbank?";
+$l['faceclaims_database_accept'] = '{1} hat deine Avatarperson <strong>"{4}"</strong> angenommen!';
+$l['myalerts_setting_faceclaims_database_accept'] = "Benachrichtigung bei Annahme vom Team bei von dir hinzugefügten Avatarpersonen für die Avatarpersonendatenbank?";
+
+
+// RANDOM PAGE
+$l['faceclaims_database_randompage_nav'] = 'Zufällige Avatarpersonen';
+$l['faceclaims_database_randompage_count'] = '{1} zufällige Avatarpersonen';
